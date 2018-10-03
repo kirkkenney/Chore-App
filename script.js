f771e5950d96f2a9bbf6ec8427e8647e10d97265
Kirk Kenney <kirk_kenney_1@hotmail.co.uk>
2018-10-03 19:20:41 +0100

    First Commit - Chore App

---
+++ script.js
@@ -0,0 +1,274 @@
+var next = document.getElementById('next');
+var prev = document.getElementById('prev');
+var list = document.getElementsByClassName('listCont');
+var openInput = document.getElementById('inputCont');
+var submit = document.getElementById('submit');
+var listCont = document.getElementsByClassName('listCont');
+
+
+window.onload = function() {
+	date = new Date();
+	currentDay = date.getDay();
+	days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+	document.getElementById('dayText').textContent = days[currentDay];
+	list[currentDay].style.display = "block";
+	var allItems = JSON.parse(localStorage.getItem("allList"));
+	if (allItems == null) allItems = [];
+	var i;
+	var x;
+	for (i = 0; i < listCont.length; i++) {
+		for (x = 0; x < allItems.length; x++) {
+			var li = document.createElement("li");
+			var button = document.createElement("button");
+			li.appendChild(document.createTextNode(allItems[x]));
+			li.appendChild(button);
+			button.innerHTML = "&#10004;"
+			listCont[i].children[0].appendChild(li);	
+		}
+	}
+	var satItems = JSON.parse(localStorage.getItem("satList"));
+	if (satItems == null) satItems = [];
+	for (i = 0; i < satItems.length; i++) {
+		var li = document.createElement("li");
+		var button = document.createElement("button");
+		li.appendChild(document.createTextNode(satItems[i]));
+		li.appendChild(button);
+		button.innerHTML = "&#10004;"
+		listCont[6].children[0].appendChild(li);		
+	}
+	var friItems = JSON.parse(localStorage.getItem("friList"));
+	if (friItems == null) friItems = [];
+	for (i = 0; i < friItems.length; i++) {
+		var li = document.createElement("li");
+		var button = document.createElement("button");
+		li.appendChild(document.createTextNode(friItems[i]));
+		li.appendChild(button);
+		button.innerHTML = "&#10004;"
+		listCont[5].children[0].appendChild(li);		
+	}	
+	var thursItems = JSON.parse(localStorage.getItem("thursList"));
+	if (thursItems == null) thursItems = [];
+	for (i = 0; i < thursItems.length; i++) {
+		var li = document.createElement("li");
+		var button = document.createElement("button");
+		li.appendChild(document.createTextNode(thursItems[i]));
+		li.appendChild(button);
+		button.innerHTML = "&#10004;"
+		listCont[4].children[0].appendChild(li);		
+	}
+	var wedsItems = JSON.parse(localStorage.getItem("wedsList"));
+	if (wedsItems == null) wedsItems = [];
+	for (i = 0; i < wedsItems.length; i++) {
+		var li = document.createElement("li");
+		var button = document.createElement("button");
+		li.appendChild(document.createTextNode(wedsItems[i]));
+		li.appendChild(button);
+		button.innerHTML = "&#10004;"
+		listCont[3].children[0].appendChild(li);		
+	}	
+	var tuesItems = JSON.parse(localStorage.getItem("tuesList"));
+	if (tuesItems == null) tuesItems = [];
+	for (i = 0; i < tuesItems.length; i++) {
+		var li = document.createElement("li");
+		var button = document.createElement("button");
+		li.appendChild(document.createTextNode(tuesItems[i]));
+		li.appendChild(button);
+		button.innerHTML = "&#10004;"
+		listCont[2].children[0].appendChild(li);		
+	}
+	var monItems = JSON.parse(localStorage.getItem("monList"));
+	if (monItems == null) monItems = [];
+	for (i = 0; i < monItems.length; i++) {
+		var li = document.createElement("li");
+		var button = document.createElement("button");
+		li.appendChild(document.createTextNode(monItems[i]));
+		li.appendChild(button);
+		button.innerHTML = "&#10004;"
+		listCont[1].children[0].appendChild(li);		
+	}
+	var sunItems = JSON.parse(localStorage.getItem("sunList"));
+	if (sunItems == null) sunItems = [];
+	for (i = 0; i < sunItems.length; i++) {
+		var li = document.createElement("li");
+		var button = document.createElement("button");
+		li.appendChild(document.createTextNode(sunItems[i]));
+		li.appendChild(button);
+		button.innerHTML = "&#10004;"
+		listCont[0].children[0].appendChild(li);		
+	}					
+	buttonToggle();
+}
+function clearStorage() {
+	localStorage.clear();
+	var ul = document.getElementsByTagName("ul");
+	var i;
+	for (i = 0; i < ul.length; i++) {
+		ul[i].innerHTML = "";
+	}
+}
+function changeDay(n) {	
+	currentDay += n;
+	if (currentDay > 6) {
+		currentDay = 0;
+	} 
+	if (currentDay < 0) {
+		currentDay = 6;
+	}
+	var i;
+	for (i = 0; i < list.length; i++) {
+		list[i].style.display = "none";
+	}
+	document.getElementById('dayText').textContent = days[currentDay];
+	list[currentDay].style.display = "block";
+}
+function inputScreen() {
+	document.getElementById('inputScreen').style.display = "block";
+	document.getElementById('close').addEventListener("click", function() {
+	document.getElementById('inputScreen').style.display = "none";	
+	document.getElementById('errorMsg').textContent = "";
+	dayChoice.style.border = "solid 1px white";
+	input.style.border = "solid 1px white";
+	});
+}
+function addChore() {
+	var input = document.getElementById('input');
+	var dayChoice = document.getElementById('dayChoice');
+	var li = document.createElement("li");
+	var button = document.createElement("button");
+	if (input.value.length == 0 || dayChoice.value == 10) {
+		return false;
+	}
+	if (dayChoice.value == 7) {
+		var i;
+		for (i = 0; i < listCont.length; i++) {
+			var li = document.createElement("li");
+			var button = document.createElement("button");
+			li.appendChild(document.createTextNode(input.value));
+			li.appendChild(button);
+			button.innerHTML = "&#10004;"
+			listCont[i].children[0].appendChild(li);
+		}
+		var allItems = JSON.parse(localStorage.getItem("allList"));
+		if (allItems == null) {
+			allItems = [];
+			}
+		allItems.push(input.value);
+		localStorage.setItem("allList", JSON.stringify(allItems));
+	} else if (dayChoice.value == 6) {
+		li.appendChild(document.createTextNode(input.value));
+		li.appendChild(button);
+		button.innerHTML = "&#10004;"
+		listCont[dayChoice.value].children[0].appendChild(li);
+		var satItems = JSON.parse(localStorage.getItem("satList"));
+		if (satItems == null) {
+			satItems = [];
+		}
+		satItems.push(input.value);
+		localStorage.setItem("satList", JSON.stringify(satItems));
+	} else if (dayChoice.value == 5) {
+		li.appendChild(document.createTextNode(input.value));
+		li.appendChild(button);
+		button.innerHTML = "&#10004;"
+		listCont[dayChoice.value].children[0].appendChild(li);
+		var friItems = JSON.parse(localStorage.getItem("friList"));
+		if (friItems == null) {
+			friItems = [];
+		}
+		friItems.push(input.value);
+		localStorage.setItem("friList", JSON.stringify(friItems));
+	} else if (dayChoice.value == 4) {
+		li.appendChild(document.createTextNode(input.value));
+		li.appendChild(button);
+		button.innerHTML = "&#10004;"
+		listCont[dayChoice.value].children[0].appendChild(li);
+		var thursItems = JSON.parse(localStorage.getItem("thursList"));
+		if (thursItems == null) {
+			thursItems = [];
+		}
+		thursItems.push(input.value);
+		localStorage.setItem("thursList", JSON.stringify(thursItems));
+	} else if (dayChoice.value == 3) {
+		li.appendChild(document.createTextNode(input.value));
+		li.appendChild(button);
+		button.innerHTML = "&#10004;"
+		listCont[dayChoice.value].children[0].appendChild(li);
+		var wedsItems = JSON.parse(localStorage.getItem("wedsList"));
+		if (wedsItems == null) {
+			wedsItems = [];
+		}
+		wedsItems.push(input.value);
+		localStorage.setItem("wedsList", JSON.stringify(wedsItems));
+	} else if (dayChoice.value == 2) {
+		li.appendChild(document.createTextNode(input.value));
+		li.appendChild(button);
+		button.innerHTML = "&#10004;"
+		listCont[dayChoice.value].children[0].appendChild(li);
+		var tuesItems = JSON.parse(localStorage.getItem("tuesList"));
+		if (tuesItems == null) {
+			tuesItems = [];
+		}
+		tuesItems.push(input.value);
+		localStorage.setItem("tuesList", JSON.stringify(tuesItems));
+	} else if (dayChoice.value == 1) {
+		li.appendChild(document.createTextNode(input.value));
+		li.appendChild(button);
+		button.innerHTML = "&#10004;"
+		listCont[dayChoice.value].children[0].appendChild(li);
+		var monItems = JSON.parse(localStorage.getItem("monList"));
+		if (monItems == null) {
+			monItems = [];
+		}
+		monItems.push(input.value);
+		localStorage.setItem("monList", JSON.stringify(monItems));
+	} else if (dayChoice.value == 0) {
+		li.appendChild(document.createTextNode(input.value));
+		li.appendChild(button);
+		button.innerHTML = "&#10004;"
+		listCont[dayChoice.value].children[0].appendChild(li);
+		var sunItems = JSON.parse(localStorage.getItem("sunList"));
+		if (sunItems == null) {
+			sunItems = [];
+		}
+		sunItems.push(input.value);
+		localStorage.setItem("sunList", JSON.stringify(sunItems));
+	}			
+	input.value = "";
+	input.placeholder = "What needs to be done?"
+	dayChoice.value = 10;
+	document.getElementById('inputScreen').style.display = "none";
+	buttonToggle();
+}
+function buttonToggle() {
+	var btn = document.querySelectorAll('li button');
+	var y;
+	for (y = 0; y < btn.length; y++) {
+		btn[y].addEventListener("click", completeTask);
+	}
+} 
+function completeTask() {
+	var i;
+	var li = document.getElementsByTagName("li");
+	var audio = document.getElementById("audio");
+	for (i = 0; i < li.length; i++) {
+		var animate = this.parentNode;
+		animate.classList.toggle("hide");
+		audio.volume = 1;
+		audio.currentTime = .5;
+		audio.play();
+		setTimeout(function() {
+			animate.style.display = "none";
+		}, 600);
+	}
+}
+
+
+
+next.addEventListener("click", function() {
+	changeDay(1);
+});
+prev.addEventListener("click", function() {
+	changeDay(-1);
+});
+openInput.addEventListener("click", inputScreen);
+submit.addEventListener("click", addChore);
+document.getElementById("clear").addEventListener("click", clearStorage);
